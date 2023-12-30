9a461a75d2d26086ba886d52e5e900484f6c0b07
stooged <stooged@users.noreply.github.com>
2023-12-30 16:10:45 +0800

    initial

---
+++ README.md
@@ -0,0 +1,41 @@
+# PS5 PI Server
+
+
+this is an install script to setup a dns server, web server and a exploit host on a raspberry pi.
+
+
+it is using <a href=https://github.com/idlesauce/PS5-Exploit-Host>PS5-Exploit-Host</a> by idlesauce.
+
+
+## Setup
+
+install an os like Raspberry Pi OS Lite
+
+boot up the pi and set your credentials then use putty or something similar to access ssh.
+
+once in ssh run the following commands:
+
+
+install git
+
+```
+sudo apt update
+sudo apt install git
+```
+
+clone this repo and enter the cloned directory
+
+```
+git clone https://github.com/stooged/PS5-PI-Server
+cd PS5-PI-Server
+```
+
+run the install script
+
+```
+chmod 777 install.sh
+sudo ./install.sh
+
+```
+
+once the pi reboots you should be able to access it on http://ps5.local
---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+sudo apt install dnsmasq -y
+sudo apt-get install nginx -y
+sudo sed -i 's/#domain-needed/domain-needed/g' /etc/dnsmasq.conf
+sudo sed -i 's/#bogus-priv/bogus-priv/g' /etc/dnsmasq.conf
+sudo sed -i 's/#expand-hosts/expand-hosts/g' /etc/dnsmasq.conf
+sudo sed -i 's^#conf-file=/etc/dnsmasq.more.conf^conf-file=/etc/dnsmasq.more.conf^g' /etc/dnsmasq.conf
+sudo openssl req -subj "/C=US/O=ps5/CN=ps5.local" -x509 -nodes -days 8192 -newkey rsa:2048 -keyout /etc/nginx/sites-available/cert.key -out /etc/nginx/sites-available/cert.crt
+sudo sed -i 's/# listen 443 ssl/ listen 443 ssl/g' /etc/nginx/sites-available/default
+sudo sed -i 's/# listen \[::\]:443 ssl/ listen \[::\]:443 ssl/g' /etc/nginx/sites-available/default
+sudo sed -i 's/server_name _/server_name ps5.local/g' /etc/nginx/sites-available/default
+sudo sed -i 's^# SSL configuration^# SSL configuration\r\n	 ssl_certificate /etc/nginx/sites-available/cert.crt;\r\n	 ssl_certificate_key /etc/nginx/sites-available/cert.key;^g' /etc/nginx/sites-available/default
+sudo cp dstart.sh /etc/
+sudo cp -r document /var/www/html/
+sudo touch /var/www/html/index.html
+echo "<html><meta HTTP-EQUIV='REFRESH' content='0; url=/document/index.html'></html>" | sudo tee -a /var/www/html/index.html
+sudo sed -i 's^"exit 0"^^g' /etc/rc.local
+sudo sed -i 's^exit 0^sudo bash ./etc/dstart.sh \& \n\nexit 0^g' /etc/rc.local
+sudo rm /etc/dnsmasq.more.conf
+IP=$(hostname -I | cut -f1 -d' ')
+echo "address=/playstation.com/127.0.0.1
+address=/manuals.playstation.net/$IP
+address=/playstation.net/127.0.0.1
+address=/playstation.org/127.0.0.1
+address=/akadns.net/127.0.0.1
+address=/akamai.net/127.0.0.1
+address=/akamaiedge.net/127.0.0.1
+address=/edgekey.net/127.0.0.1
+address=/edgesuite.net/127.0.0.1
+address=/llnwd.net/127.0.0.1
+address=/scea.com/127.0.0.1
+address=/sonyentertainmentnetwork.com/127.0.0.1
+address=/ribob01.net/127.0.0.1
+address=/cddbp.net/127.0.0.1
+address=/nintendo.net/127.0.0.1
+address=/ea.com/127.0.0.1" | sudo tee -a /etc/dnsmasq.more.conf
+sudo sed -i 's^raspberrypi^ps5^g' /etc/hosts
+sudo sed -i 's^raspberrypi^ps5^g' /etc/hostname
+echo "Install complete, Rebooting"
+sudo reboot
+
+
+
+
+
+
+
+
+
+
+
