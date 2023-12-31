4e62f981981d61fa5dfc243eb330e0ebe1bf0029
stooged <stooged@users.noreply.github.com>
2023-12-31 13:33:58 +0800

    Update setup_wifiAP.sh

--- setup_wifiAP.sh
+++ setup_wifiAP.sh
@@ -16,9 +16,11 @@ wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP CCMP
 rsn_pairwise=CCMP" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" | sudo tee -a /etc/default/hostapd
-sudo sed -i 's^exit 0^sudo iptables-restore < ./etc/iptables.ipv4.nat\r\n\sudo ifconfig wlan0 up\r\nsudo ./sbin/hostapd /etc/hostapd/hostapd.conf \& \n\nexit 0^g' /etc/rc.local
+sudo sed -i 's^exit 0^sudo iptables-restore < /etc/iptables.ipv4.nat\r\n\sudo ifconfig wlan0 up\r\nsudo ./sbin/hostapd /etc/hostapd/hostapd.conf \& \n\nexit 0^g' /etc/rc.local
 sudo sed -i 's^#net.ipv4.ip_forward=1^net.ipv4.ip_forward=1^g' /etc/sysctl.conf
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
 sudo netfilter-persistent save
 sudo service dhcpcd restart
