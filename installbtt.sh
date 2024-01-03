560f11f2486a2c3fcc16f711e1726a40f636a6a0
stooged <stooged@users.noreply.github.com>
2024-01-03 14:08:01 +0800

    Update installbtt.sh

--- installbtt.sh
+++ installbtt.sh
@@ -114,11 +114,12 @@ break;;
 esac
 done
 echo '#!/bin/bash
+sleep 5
 sudo systemctl stop dnsmasq.service
 sudo systemctl stop dhcpcd.service
 sudo sysctl net.ipv4.ip_forward=1
 sudo iptables -t nat -A POSTROUTING -s 10.0.0.0/24 ! -d 10.0.0.0/24 -j MASQUERADE
-sudo nmcli dev wifi hotspot ifname wlan1 ssid '$APSSID' password "'$APPSWD'"
+sudo nmcli dev wifi hotspot ifname wlan1 ssid '$APSSID' password '$APPSWD'
 sudo nmcli device modify wlan1 ipv4.method disabled
 sudo nmcli device modify wlan1 ipv6.method disabled
 sudo systemctl start dhcpcd.service
